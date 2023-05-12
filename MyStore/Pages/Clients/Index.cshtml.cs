235d9e558ed4b0e9b0b59de1fe7c02dcecab8922
jqureshi <jawwadq00@gmail.com>
2023-05-12 13:58:47 -0400

    Add project files.

---
+++ MyStore/Pages/Clients/Index.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace MyStore.Pages.Clients
+{
+    public class IndexModel : PageModel
+    {
+        public List<ClientInfo> listOfClients = new List<ClientInfo>();
+        public void OnGet()
+        {
+            try
+            {
+                string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=mystore;Integrated Security=True";
+
+                using(SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                }
+            }
+            catch(Exception ex) { }
+
+        }
+    }
+
+    public class ClientInfo
+    {
+        public string id;
+        public string name;
+        public string email;
+        public string phone;
+        public string address;
+    }
+}
