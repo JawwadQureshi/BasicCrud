fd805b5b1aae07f3a7f52eb5495c1a8305b20ce5
jqureshi <jawwadq00@gmail.com>
2023-05-12 14:04:36 -0400

    Comments

--- MyStore/Pages/Clients/Index.cshtml.cs
+++ MyStore/Pages/Clients/Index.cshtml.cs
@@ -16,21 +16,30 @@ namespace MyStore.Pages.Clients
 
                 using(SqlConnection connection = new SqlConnection(connectionString))
                 {
+                    //Opens SQL connection
                     connection.Open();
+
+                    //Reads Data from all the rows in the clients table
                     String sql = "SELECT * FROM clients";
+
+                    //Executes SQL Query
                     using (SqlCommand command = connection.CreateCommand())
                     {
+                        //Obtain SQL Data Reader
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
+                            //Read the data from the table
                             while (reader.Read())
                             {
+                                //Save data into a client info object
                                 ClientInfo clientInfo = new ClientInfo();
+
+                                //Id is of type string, but in database it is integer, so must add "" to convert
                                 clientInfo.id = "" + reader.GetInt32(0);
                                 clientInfo.name = reader.GetString(1);
                                 clientInfo.email = reader.GetString(2);
                                 clientInfo.phone = reader.GetString(3);
                                 clientInfo.address = reader.GetString(4);
-                                clientInfo.created_at = reader.GetDateTime(5).ToString();
                             }
                         }
                     }
